9689d3492d8a010fe8c9b3c3595dc6bf817031dc
taru <taruru1010@gmail.com>
2020-12-01 02:39:37 +0900

    fix __init__.py

--- MSR/__init__.py
+++ MSR/__init__.py
@@ -1,4 +1,4 @@
 from .setup import setup
 from .audio_convert import wavToRaw
 from .result import execRecognition
-from .speakers import speakerDescriptions
+from .speakers import speakerDescription
